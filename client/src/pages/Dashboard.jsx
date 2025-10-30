c1c490ce6e5d242cd8c515c735c99034222b885f
FeMolz <felipeborbamolz@gmail.com>
2025-10-30 10:47:55 -0300

    configuração de ambiente 2.0

---
+++ client/src/pages/Dashboard.jsx
@@ -0,0 +1,12 @@
+const react = require('react');
+
+function Dashboard() {
+  return (
+    <div>
+      <h1>Página Inicial</h1>
+      <p>Este é o conteúdo da seção Home.</p>
+    </div>
+  );
+}
+
+export default Dashboard
